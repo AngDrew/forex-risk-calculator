353808c969d9af95d8c578014b4824a055ce7d41
AngDrew <andrewanggada@gmail.com>
2023-09-01 01:31:37 +0700

    feat: implemented fx risk calculator

---
+++ lib/src/widgets/default_app_bar.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class DefaultAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const DefaultAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Text('Forex Risk Calculator'),
+      centerTitle: true,
+      actions: <Widget>[
+        IconButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: const Text('About'),
+                  content: const SelectableText(
+                    'This is a simple risk '
+                    'calculator for Forex. '
+                    '\nIt is not a financial advice. '
+                    'Use at your own risk.'
+                    '\nBy: github.com/AngDrew',
+                  ),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('OK'),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+          icon: const Icon(Icons.info_rounded),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size(double.infinity, kToolbarHeight);
+}
